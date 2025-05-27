81e0752d81e978a6e17b7f915ecf2eaa640b80ba
Matteus Moreno <matteus_moreno@live.com>
2025-05-27 16:27:05 -0300

    initial commit

---
+++ src/main/java/br/com/matteusmoreno/my_audio_to_text_backend/MyAudioToTextBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.my_audio_to_text_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAudioToTextBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAudioToTextBackendApplication.class, args);
+	}
+
+}
